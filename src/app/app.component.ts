34bb1d1dc48f1c8bfccbe3dfca155f06339cffe3
sabrina hernandez <sabrinaguzman2009@gmail.com>
2023-07-10 17:32:50 -0400

    add bootstrap, buttons to edit and delete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+import { Employee } from './models/employee';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-simple-crud';
+  employeeArray: Employee[] = [
+    { id: 1, name: "Ryan", country: "USA" },
+    { id: 2, name: "Mirriam", country: "USA" },
+    { id: 3, name: "Amanda", country: "USA" }
+  ];
+
+  selectedEmployee: Employee = new Employee();
+
+  openForEdit(employee: Employee){
+    this.selectedEmployee = employee;
+  }
+
+  addOrEdit(){
+    if(this.selectedEmployee.id === 0){
+      this.selectedEmployee.id = this.employeeArray.length + 1;
+      this.employeeArray.push(this.selectedEmployee);
+    }
+    this.selectedEmployee = new Employee();
+  }
+
+  delete(){
+    if(confirm('Are you sure you want to delete it?')){
+      this.employeeArray = this.employeeArray.filter(x => x != this.selectedEmployee);
+      this.selectedEmployee = new Employee();
+    }
+  }
 }
---
+++ src/app/models/employee.spec.ts
@@ -0,0 +1,7 @@
+import { Employee } from './employee';
+
+describe('Empoyee', () => {
+  it('should create an instance', () => {
+    expect(new Employee()).toBeTruthy();
+  });
+});
